a2f7333dd170ac49589447f3fa89b5611b4fb636
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-06-12 11:55:11 +0545

    custom authentication backend

--- chautari/settings.py
+++ chautari/settings.py
@@ -132,3 +132,9 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'dashboard'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+AUTHENTICATION_BACKENDS = [
+    'account.authentication.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
